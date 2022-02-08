019dbcf3dd621ff59b18ff0a09a5af9652da4446
Thanat Mathanakraw <phooon884@gmail.com>
2022-02-08 18:15:52 +0800

    finsih lstm

--- project/political classification/reddit_dataset.py
+++ project/political classification/reddit_dataset.py
@@ -1,42 +1,55 @@
-import pandas as pd    
+import pandas as pd
 from torch.utils.data import Dataset
 import torchtext.data as ttd
 from torchtext.vocab import GloVe
 import torch.nn as nn
 import torch
+from sklearn.model_selection import train_test_split
+import numpy as np
+from torch.utils.data import DataLoader
+
 
 class PoliticalStancesDataset(Dataset):
-    def __init__(self, csv_file, tensor_dir, encoded = True):
-            if encoded is True:
-                self.X = torch.load(tensor_dir)
-            if encoded is False:
-                df = pd.read_csv(csv_file)
-                df['classification'] = df['classification'].map({'left': 0, 'right': 1})
-                tonkenized_df = pd.DataFrame()
-                tokenizer = ttd.utils.get_tokenizer("toktok")
-                for _ , row in df.iterrows(): 
-                    js = {
-                        "text": tokenizer(row["text"]),
-                        "classification": row["classification"]
-                    }
-                    tonkenized_df = tonkenized_df.append(js,ignore_index = True)
-                self.vec  = GloVe("twitter.27B",dim=25)
-                length = 50
-                self.X = torch.zeros(len(df),length,25)
-                for index , row in df.iterrows(): 
-                    ret = self.vec.get_vecs_by_tokens(tonkenized_df.iloc[index]["text"], lower_case_backup=True)
-                    ret = self.__getvec(ret, 50)
-                    self.X[index, :,:] = ret
-                torch.save(self.X,tensor_dir)
+    def __init__(self, csv_file, train, random_state=42):
+        df = pd.read_csv(csv_file, index_col=0)
+        df['classification'] = df['classification'].map(
+            {'left': 0, 'right': 1})
+        self.text = df["text"].to_numpy()
+        self.classification = df["classification"].to_numpy()
+        if train is True:
+            self.text, _, self.classification, _ = train_test_split(
+                self.text, self.classification, test_size=0.25, random_state=random_state)
+        else:
+            _,  self.text, _, self.classification = train_test_split(
+                self.text, self.classification, test_size=0.25, random_state=random_state)
+        self.tokenizer = ttd.utils.get_tokenizer("toktok")
+        self.vec = GloVe("twitter.27B", dim=25)
+        self.X = torch.zeros(50, 25)
 
     def __len__(self):
-        return len(self.df)
+        return len(self.classification)
 
-    def __getitem__(self):
-       return self.X
+    def __getitem__(self, idx):
+        item_tonkenized = self.tokenizer(self.text[idx])
+        X = self.vec.get_vecs_by_tokens(
+            item_tonkenized, lower_case_backup=True)
+        X = self.__getvec(X, 50)
+        return X, torch.Tensor([int(self.classification[idx])])
 
-    def __getvec(tensor, length):
+    def __getvec(self, tensor, length):
         if tensor.size()[0] > length:
-            return tensor[0:length,:]
+            return tensor[0:length, :]
         m = nn.ZeroPad2d((0, 0, length-tensor.size()[0], 0))
         return m(tensor)
+    def embed(self,text):
+        item_tonkenized = self.tokenizer(text)
+        X = self.vec.get_vecs_by_tokens(
+            item_tonkenized, lower_case_backup=True)
+        X = self.__getvec(X, 50)
+        
+if __name__ == "__main__":
+    test = PoliticalStancesDataset("./dataset_finalized.csv", train=True)
+    loader = DataLoader(test, batch_size=64, shuffle=True)
+    for _, i in loader:
+        print(i)
+        break
